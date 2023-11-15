83991fdba2d31140f7ae499c37767fd69592f91c
bilginkilic <blgnklc@gmail.com>
2023-11-16 02:13:55 +0300

    akustik

--- Wizard.js
+++ Wizard.js
@@ -6,6 +6,8 @@ import { useLocalStorage } from './useLocalStorage';
 import { GlobalContext } from './GlobalContext';
 import { Statisticx, Todo } from './src/models';
 import { DataStore, Predicates } from "@aws-amplify/datastore";
+import questionsw from './questionsData'
+
 const Wizard = () => {
   const { takeTest } = React.useContext(AuthContext);
   const [questionIndex, setQuestionIndex] = useLocalStorage('questionIndex', 0);
@@ -17,98 +19,7 @@ const Wizard = () => {
   const [username, setUsername] = useLocalStorage('username', '');
 
 
-  const [questionsw, setQuestionsw] = useState([
-    {
-      id: 1,
-      text: 'Do you use a dishwasher to wash your dishes?',
-      options: [
-        { text: 'Yes', valueSaving: 111, valueTotal: 15, task: 'Always washing in dishwasher, congratulations!', category: 'Dishwashing', type: 'Achievement' },
-        { text: 'No', valueSaving: 0, valueTotal: 126, task: 'Need to wash in dishwasher, take the necessary action! ', category: 'Dishwashing', type: 'Task' },
-      ],
-    },
-    {
-      id: 2,
-      text: 'Do you rinse the dishes before putting them in the machine?',
-      options: [
-        { text: 'Yes', valueSaving: 0, valueTotal: 36, task: 'Do not waste water for rinsing, congratulations!', category: 'Dishwashing', type: 'Task' },
-        { text: 'No', valueSaving: 21, valueTotal: 15, task: 'Do you really need to rinse, take the necessary action!', category: 'Dishwashing', type: 'Achievement' },
-      ],
-    },
-    {
-      id: 3,
-      text: 'How do you run your dishwasher, full or half full?',
-      options: [
-        { text: 'Full', valueSaving: 11, valueTotal: 22, task: 'You made a significant effect by full loading of your dishwasher, congratulations!', category: 'Dishwashing', type: 'Achievement' },
-        { text: 'Half full', valueSaving: 0, valueTotal: 22, task: 'Do you know full loading your dishwasher saves 11 liters for each run, take the necessary action! ', category: 'Dishwashing', type: 'Task' },
-      ],
-    },
-    {
-      id: 4,
-      text: 'Do the faucets in your house have slow-flows?',
-      options: [
-        { text: 'Yes', valueSaving: 44, valueTotal: 76, task: 'You made a significant effect by full loading of your dishwasher, congratulations!', category: 'Plumbing', type: 'Achievement' },
-        { text: 'No', valueSaving: 0, valueTotal: 120, task: 'You can save water with an easy arrangement of slow-flows, take the necessary action! ', category: 'Plumbing', type: 'Task' },
-      ],
-    },
-    {
-      id: 5,
-      text: 'How many minutes you take a shower?',
-      options: [
-        { text: 'Under 5 mins', valueSaving: 160, valueTotal: 70, task: 'Bucket usage or 5 minutes shower is the target and it is a hard challenge, congratulations!', category: 'Shower', type: 'Achievement' },
-        { text: '5 - 10 mins', valueSaving: 80, valueTotal: 150, task: 'Decreasing your shower duration makes a significant difference, take the necessary action! ', category: 'Shower', type: 'Task' },
-        { text: '11 - 15 mins', valueSaving: -20, valueTotal: 250, task: 'Decreasing your shower duration makes a significant difference, take the necessary action! ', category: 'Shower', type: 'Task' },
-        { text: 'Over 15 mins', valueSaving: -220, valueTotal: 450, task: 'Decreasing your shower duration makes a significant difference, take the necessary action! ', category: 'Shower', type: 'Task' },
-        { text: 'Use a bucket', valueSaving: 210, valueTotal: 20, task: 'Bucket usage or 5 minutes shower is the target and it is a hard challenge, congratulations!', category: 'Shower', type: 'Achievement' }
-      ],
-    },
-    {
-      id: 6,
-      text: 'How do you laundry, full or half full?',
-      options: [
-        { text: 'Full', valueSaving: 90, valueTotal: 180, task: 'By full loading your laundry you save 90 liters water , congratulations!', category: 'Laundry', type: 'Achievement' },
-        { text: 'Half full', valueSaving: 0, valueTotal: 180, task: '90 liters more with full loaded laundries, take the necessary action!', category: 'Laundry', type: 'Task' },
-      ],
-    },
-    {
-      id: 7,
-      text: 'Do you turn off the water while you are brushing?',
-      options: [
-        { text: 'Yes', valueSaving: 2, valueTotal: 2, task: 'You saved 10 liters for your each brush , congratulations!', category: 'Daily activities', type: 'Achievement' },
-        { text: 'No', valueSaving: 0, valueTotal: 4, task: 'Turn off the water and save 10 liters more for your each brush, take the necessary action!', category: 'Daily activities', type: 'Task' },
-      ],
-    },
-    {
-      id: 8,
-      text: 'Does your faucets/pipes leak?',
-      options: [
-        { text: 'Yes', valueSaving: 0, valueTotal: 32, task: 'You fixed your leaky faucets, congratulations!', category: 'Plumbing', type: 'Task' },
-        { text: 'No', valueSaving: 32, valueTotal: 0, task: '32 liters more with only one leaky faucet, take the necessary action!  ', category: 'Plumbing', type: 'Achievement' },
-      ],
-    },
-    {
-      id: 9,
-      text: 'Do you have a car?',
-      options: [
-        { text: 'No', valueSaving: 0, valueTotal: 0 },
-
-        { text: 'Yes', valueSaving: 0, valueTotal: 0 },
-      ]
-
-    },
-    {
-      id: 10,
-      text: 'Do you wash your car yourself or do you use pressure washer system?',
-      options: [
-
-
-        { text: 'Yourself', valueSaving: 0, valueTotal: 200, task: '120 liters more with each car wash, take the necessary action!', category: 'Car owners', type: 'Task' },
-        { text: 'pressure washer system', valueSaving: 120, valueTotal: 80, task: 'Washing your car by pressure washer systems you saved 120 liters water , congratulations!', category: 'Car owners', type: 'Achievement' },
-      ]
-
-    }
-
-
-  ]);
+  
 
   useEffect(() => {
 
